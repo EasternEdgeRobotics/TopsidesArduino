caeb107d8c327de3512106b7fc119bcb855feab0
Tybalt Lea <tybalt@fastmail.com>
2016-03-24 22:02:24 -0230

    Updated code to make a button write back

--- TopsidesArduino2015_01_16/TopsidesArduino2015_01_16.ino
+++ TopsidesArduino2015_01_16/TopsidesArduino2015_01_16.ino
@@ -1,26 +1,37 @@
-#define READ_DIGITAL 1
-#define READ_ANALOG 2
-#define WRITE 3
 
-int inputPin[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23 };            // an array of pin numbers to be used as digital inputs (buttons)
+int inputPin[] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25 };            // an array of pin numbers to be used as digital inputs (buttons), starting at 2 because 0,1 are used for hardware serial
 
-int outputPin[] = {24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47 }; // an array of pin numbers to be used as digital outputs (lights)
+int outputPin[] = {26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49 }; // an array of pin numbers to be used as digital outputs (lights)
   
-int analogPin[] = {A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14, A15 };                           // an array of pin numbers to be used as analog inputs (pots)
+//int analogPin[] = {A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14, A15 };                           // an array of pin numbers to be used as analog inputs (pots)
 
 int baud = 19200;																					// rate of serial communication to topsides (9600 - 115200)
 
 void setup() {
   Serial.begin(baud);																				// begin communication over the serial-USB line at selected baud
-  for (int thisPin = 0; thisPin < 24; thisPin++)  {													// initialize each physical inputPin as a digital input
+  for (int thisPin = 2; thisPin < 26; thisPin++)  {													// initialize each physical inputPin as a digital input
     pinMode(inputPin[thisPin], INPUT);
   }
   
-  for (int thisPin = 0; thisPin < 24; thisPin++)  {													// initialize each physical outputPin as a digital output
+  for (int thisPin = 26; thisPin < 50; thisPin++)  {													// initialize each physical outputPin as a digital output
     pinMode(outputPin[thisPin], OUTPUT); 
   }
 }
 
+
+void loop() {
+  check_button();                                               //call to check for button input
+}
+
+void check_button() {
+  for (int i=2; i < 26; i++) {
+    Serial.println (digitalRead (inputPin[i]),DEC);                   //read each input pin and write the state to the serial line
+    delay(500);
+    //Serial.flush();                                             //flush the serial line between pins (each pin is sent as one full byte)
+  }
+}
+
+/*
 void loop() {
   if (!Serial.available()) {																		// if serial is not available, continue looping here
     return;
@@ -47,3 +58,4 @@ void loop() {
     }
   }
 }
+*/
